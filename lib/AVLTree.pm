67bd9c3caa23931f7c9c5e6f7ee8d8e5d7cd0384
Alessandro Vullo <avullo@ebi.ac.uk>
2017-12-20 08:03:59 +0000

    Switch to semantic versioning.

--- lib/AVLTree.pm
+++ lib/AVLTree.pm
@@ -4,7 +4,7 @@ use 5.008;
 use strict;
 use warnings;
 
-our $VERSION = '0.01';
+our $VERSION = '0.0.2';
 our $ENABLE_DEBUG = 0;
 
 require XSLoader;
@@ -19,7 +19,7 @@ AVLTree - Perl extension for efficient creation and manipulation of AVL balanced
 
 =head1 VERSION
 
-Version 0.01
+Version 0.0.2
 
 =head1 DESCRIPTION
 
